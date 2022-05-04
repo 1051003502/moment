6fe5b4dd9045bac5690df68d6ffaa80bd5d083b4
XinZhao <33519346+1051003502@users.noreply.github.com>
2022-05-04 17:11:54 +0800

    Delete test01 directory

--- test01/Main.py
+++
@@ -1,7 +0,0 @@
-import function
-if __name__ == '__main__':
-    test_data=[['zhaoxin','买了手机'],['junfeng','2b'],['dog','bone'],['周鑫','guapi'],['周鹏飞','Game']\
-                ,['panda','竹子']]
-    train,test=function.splitData(test_data,2,0,3)
-    print(train)
-    print(test)
--- test01/测试语法.py
+++
@@ -1,5 +0,0 @@
-a={5:6,7:8}
-print(5 in a)
-print(6 in a)
-
-
